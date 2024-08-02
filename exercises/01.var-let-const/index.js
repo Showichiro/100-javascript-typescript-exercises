83b3ef091a197dbefca91095226b5ddec607f3e3
uchihori <show2305@gmail.com>
2024-08-03 02:24:07 +0900

    01.var-let-const

---
+++ exercises/01.var-let-const/index.js
@@ -0,0 +1,17 @@
+// 以下のコードをリファクタリングしてください。
+// 不必要な再代入を避け、適切なスコープを設定し、const と let を正しく使用してください。
+var count = 0;
+var maxCount = 10;
+var message = "";
+
+for (var i = 0; i < maxCount; i++) {
+    count = count + 1;
+    if (count % 2 === 0) {
+        message = "Even";
+    } else {
+        message = "Odd";
+    }
+    console.log(message);
+}
+
+console.log("Final count: " + count);
