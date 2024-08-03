3846fde975b8c1ae33bf99ba7c71bd2c9d550587
uchihori <show2305@gmail.com>
2024-08-03 23:00:08 +0900

    add solutions

--- exercises/04.array/index.js
+++ exercises/04.array/index.js
@@ -6,22 +6,40 @@ export let fruits = ["apple", "banana"];
 /**
  * 引数にとったフルーツを配列に追加する.
  */
-export const addNewFruit = () => {};
+export const addNewFruit = (fruit) => {
+    fruits.push(fruit);
+};
 
 /** 指定されたインデックスのフルーツを配列から削除する. */
-export const deleteFruit = () => {};
+export const deleteFruit = (index) => {
+    const result = [];
+    for (const i in fruits) {
+        if (index !== Number(i)) {
+            result.push(fruits[i]);
+        }
+    }
+    fruits = result;
+};
 
 /**
  * 指定されたインデックスのフルーツを配列から取得する.
  * 元のフルーツ配列は変化しない.
  * 存在しないインデックスを指定した場合はnullを返却する.
  */
-export const getById = () => {};
+export const getById = (index) => {
+    return fruits.at(index) ?? null;
+};
 
 /**
  * 第一引数にインデックスを取り、第二引数に指定された値でフルーツを置き換える.
  */
-export const editFruit = () => {};
+export const editFruit = (index, fruit) => {
+    for (const i in fruits) {
+        if (index === Number(i)) {
+            fruits[i] = fruit;
+        }
+    }
+};
 
 // 以下は編集しない
 export const resetFruit = () => {
