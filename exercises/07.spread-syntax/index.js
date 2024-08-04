97d4bfb4a0464fb4f0de43e2b78012e10a75618a
uchihori <show2305@gmail.com>
2024-08-05 01:05:59 +0900

    add: solutions

--- exercises/07.spread-syntax/index.js
+++ exercises/07.spread-syntax/index.js
@@ -1,11 +1,19 @@
 // 問題1:配列の一番最初の要素を取り除いた配列を返却する関数
 // 非破壊的メソッドとして実装すること
-export const omitFirst = (arr) => {};
+export const omitFirst = (arr) => {
+    const [_, ...rest] = arr;
+    return rest;
+};
 
 // 問題2:配列の一番最後の要素を取り除いた配列を返却する関数
 // 非破壊的メソッドとして実装すること
-export const omitLast = (arr) => {};
+export const omitLast = (arr) => {
+    const [_, ...rest] = [...arr].reverse();
+    return rest.reverse();
+};
 
 // 問題3: 二つのオブジェクトをマージしたオブジェクトを返却する関数
 // 非破壊的メソッドとして実装すること
-export const mergeObject = (obj1, obj2) => {};
+export const mergeObject = (obj1, obj2) => {
+    return { ...obj1, ...obj2 };
+};
