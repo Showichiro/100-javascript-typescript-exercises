76d70f4afee6b63c663c01db7a3d7b801b19370e
uchihori <show2305@gmail.com>
2024-08-05 00:18:53 +0900

    06.class

--- exercises/06.class/index.js
+++ exercises/06.class/index.js
@@ -3,4 +3,22 @@
 // `addPet`メソッドを実装して、新しいペットを登録します。登録されたペットはオブジェクトとして保存し、ペットの種類(type)、名前(name)、年齢(age)を管理します。
 // `removePet`メソッドを実装して、登録されているペットを削除します。ペットの名前を引数に取り、該当するペットを削除します
 // TODO:
-export class PetShop {}
+export class PetShop {
+  constructor(pets) {
+    this.pets = pets ?? {};
+  }
+
+  addPet(pet) {
+    this.pets[pet.name] = pet;
+  }
+
+  removePet(name) {
+    const removed = {};
+    for (const n in this.pets) {
+      if (n !== name) {
+        removed[n] = this.pets[n];
+      }
+    }
+    this.pets = removed;
+  }
+}
